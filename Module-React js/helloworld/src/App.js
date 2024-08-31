51682146b55b5e2afddb2a70eb47c193872c9c75
Sanyamjn03 <151984925+Sanyamjn03@users.noreply.github.com>
2024-08-31 12:22:44 +0530

    Add files via upload

---
+++ Module-React js/helloworld/src/App.js
@@ -0,0 +1,12 @@
+import Helloworld from "./Component/Helloworld";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Helloworld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Module-React js/helloworld/src/Component/Helloworld.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+
+function Helloworld() {
+  return (
+    <div class="container">
+      <div style={{backgroundColor:"black",marginTop:"-8.5px",color:"skyblue",boxSizing:"border-box",textAlign:"center",height:"400px",width:"1548px",padding:"8px",marginLeft:"-8px",fontSize:"40px",fontStyle:"normal"}}> <h1>React JS</h1>
+       <img src="https://download.logo.wine/logo/React_(web_framework)/React_(web_framework)-Logo.wine.png" style={{height:"200px",marginLeft:"-10px",marginTop:"-30px"}}></img>
+       <div style={{backgroundColor:"skyblue",height:"230px",width:"1550px",marginTop:"11px",marginLeft:"-10px",color:"black",fontSize:"40px",fontSize:"60px",paddingTop:"120px"}}>
+        Hello World
+       </div>
+       </div>
+
+      
+        
+    </div>
+  );
+}
+
+export default Helloworld;
